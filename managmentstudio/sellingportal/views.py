87494b46e309d5c425f1dff1d8b70ec489231a71
AbuOmar373 <ww5z@hotmail.com>
2017-01-31 19:16:03 +0300

    Update-Lesson-8

--- managmentstudio/sellingportal/views.py
+++ managmentstudio/sellingportal/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from sellingportal import models
+
 def Index(request):
     context={'name':'Manei',
             'age':26,
@@ -10,7 +13,11 @@ def Index(request):
     #return HttpResponse('welcome to index page')
 
 def Student(request):
-    return HttpResponse('welcome to Student page')
+    students=models.Student.objects.all()
+    context ={
+        'students':students
+    }
+    return render(request, 'students.html', context)
 
 
 def StudenDegree(request,student_id):
